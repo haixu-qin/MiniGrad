bcbb9434b9a09e181b5460b83267b8b3be9baf18
haixu-qin <69027122+haixu-qin@users.noreply.github.com>
2023-09-30 19:56:31 +0000

    2

--- example.py
+++ example.py
@@ -13,7 +13,7 @@ nn.train(x_train, y_train, epochs=5100)
 """
 
 nn = DeepFeedforwardNetwork(2, [3]*2+[1], learning_rate=0.008) # Note that RAdam uses a diff lr, e.g. 0.001 
-nn.train(x_train, y_train, epochs=2000) # less epochs 
+nn.train(x_train, y_train, epochs=2000) #less epochs 
 
 y_pred = nn.pred(x_train)
 print(y_pred) #[0,1,1,0]
